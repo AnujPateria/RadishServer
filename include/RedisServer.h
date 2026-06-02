bf4a32173abf66185d9e2a5549500c00d6e20e1e
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-06-02 12:55:15 +0530

    Update RedisServer.h

--- include/RedisServer.h
+++ include/RedisServer.h
@@ -1,8 +1,22 @@
-#ifndef REDIS_SERVER_H
-#define REDIS_SERVER_H
+#ifndef REDIS_SERVER_H // |-      // use macros case
+#define REDIS_SERVER_H // |-| | -  This is called header guards in c/cpp
+/*
+Header guard : it do like if we define that file in different .cpp file then it will create same class aagin and again so redefinition of class error occurs
+
+// with guard : first include -> not define -> include file then #define marks it as already included . second include-> alrady defiend -> skip entire file
+
+Alternative-/
+
+Modern C++:
+
+#pragma once
+
+Simpler and widely used.
+*/
 
 #include <string>
-#include <atomic>
+#include <atomic> // use for thread safe varible: if not multiple thread read wrong value 
+//it guarante safe concurrent read/write
 
 class RedisServer {
 public:
@@ -13,6 +27,11 @@ public:
 private:
     int port;
     int server_socket;
+    /*
+            In linux everything is file descriptor: os assign some id to opened file
+            socket is like a file in linux
+            socket: create communication endpoint
+        */
     std::atomic<bool> running;
 
     // Setup signal handling for graceful shutdown (ctrl + c) 
